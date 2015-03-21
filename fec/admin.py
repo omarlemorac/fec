bef870bef06e5fa6765ac95921841f174050c03f
Carlos Santin <carlos@kabal.co>
2015-03-21 02:19:43 -0500

    [ADD] Reordering FIles. Added the new app for Log in Users

--- fec/admin.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from django.contrib import admin
-from fec.models import l10n_ec_authorization, l10n_ec_partner
-
-admin.site.register(l10n_ec_authorization)
-admin.site.register(l10n_ec_partner)
-
--- fec/lexus/conn.py
+++
@@ -1,8 +0,0 @@
-import pyodbc
-
-dsn = 'lexus_desarrollo'
-user = 'sa'
-password = 'sa'
-database = 'ISOFTEC_CDP'
-
-con_string = 'DSN=%s;UID=%s;PWD=%s;DATABASE=%s;' % (dsn, user, password, database)
