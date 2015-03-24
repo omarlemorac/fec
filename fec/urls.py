5ac0e1ae0c3a3d0661a031a3060a3f4405913a67
Carlos Santin <carlos@kabal.co>
2015-03-24 18:19:52 -0500

    [ADD] Login Page successfully working

--- fec/urls.py
+++ fec/urls.py
@@ -7,10 +7,14 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^admin/', include(admin.site.urls)), 
-	url(r'^login/$', authview.login_user, name="login"), 
+	url(r'^admin/', include(admin.site.urls)),
+	#url(r'^main/$', ),
+	url(r'^login/$', authview.login_user, name="login"),
 )
 
 urlpatterns += patterns('',
-	url(r'^static/(.*)$', 'django.views.static.serve', {'document_root': STATIC_ROOT, 'show_indexes': True}),
+	url(r'^static/(.*)$',
+		'django.views.static.serve',
+		{'document_root': STATIC_ROOT, 'show_indexes': True}
+	),
 )
